5966409062a6a5c3991fabb8ec45c7e680e3df32
Anthony <thewarparmoury@gmail.com>
2023-10-31 23:28:03 -0500

    Added more movies to the movie list

--- Data/Context.cs
+++ Data/Context.cs
@@ -19,7 +19,15 @@ namespace ContextExample.Data
                 new Movie { Id = 1, Title = "Toy Story" },
                 new Movie { Id = 2, Title = "Star Wars" },
                 new Movie { Id = 3, Title = "Oceans 11" },
-                new Movie { Id = 3, Title = "Oceans 12" }
+                new Movie { Id = 4, Title = "Oceans 12" },
+                new Movie { Id = 5, Title = "Predator" },
+                new Movie { Id = 6, Title = "Aliens" },
+                new Movie { Id = 7, Title = "Commando" },
+                new Movie { Id = 8, Title = "Bloodsport" },
+                new Movie { Id = 9, Title = "Terminator 2: Judgement Day" },
+                new Movie { Id = 10, Title = "Total Recall" },
+                new Movie { Id = 11, Title = "Night of the Living Dead" },
+                new Movie { Id = 12, Title = "Day of the Dead" }
             };
         }
 
